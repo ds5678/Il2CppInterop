669fd88cc34964821c1ebb69f0f8f434c38961ed
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-04-28 12:36:14 -0700

    Convert struct generator from library to CLI

---
+++ Il2CppInterop.StructGenerator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Il2CppInterop.StructGenerator;
+
+internal static class Program
+{
+    internal static void Main(string[] args)
+    {
+        // Directory that contains libil2cpp headers. Directory must contains subdirectories named after libil2cpp version.
+        var headersDirectory = args[0];
+
+        // Directory to write managed struct wrapper sources to
+        var outputDirectory = args[1];
+
+        Il2CppStructWrapperGeneratorOptions options = new(headersDirectory, outputDirectory, null);
+        Il2CppStructWrapperGenerator.Generate(options);
+    }
+}
