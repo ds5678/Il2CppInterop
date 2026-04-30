e1e24043fe78c6ee71a75a0e212d3afc9ff354f6
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-04-30 12:50:38 -0700

    Fix IIl2CppType<>.AssemblyName not including the dll extension by default

--- Il2CppInterop.Common/Attributes/InjectedTypeAttribute.cs
+++ Il2CppInterop.Common/Attributes/InjectedTypeAttribute.cs
@@ -6,6 +6,13 @@
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
 public sealed class InjectedTypeAttribute : Attribute
 {
+    /// <summary>
+    /// The name of the type. If not specified, it will be inferred from the managed type's name.
+    /// </summary>
     public string? Name { get; init; }
+    /// <summary>
+    /// The file name of the assembly that the type is defined in.
+    /// If not specified, the assembly name will be inferred from the type's containing assembly.
+    /// </summary>
     public string? Assembly { get; init; }
 }
