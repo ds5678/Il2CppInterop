525a315d7486dd355e4ae6c9e8b81e7b198d0381
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-04-30 20:20:19 -0700

    Move EnumUnderlyingType and CodeGenEnumElement into their own files

---
+++ Il2CppInterop.StructGenerator/CodeGen/CodeGenEnumElement.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+namespace Il2CppInterop.StructGenerator.CodeGen;
+
+internal class CodeGenEnumElement
+{
+    public CodeGenEnumElement(string name, string? value = null)
+    {
+        Name = name;
+        Value = value;
+    }
+
+    public string Name { get; }
+    public string? Value { get; }
+
+    public string BuildFrom(CodeGenEnum origin)
+    {
+        StringBuilder builder = new($"{origin.IndentInner}{Name}");
+        if (Value != null) builder.Append($" = {Value}");
+        builder.Append(',');
+        return builder.ToString();
+    }
+
+    public static bool operator !=(CodeGenEnumElement lhs, CodeGenEnumElement rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    public static bool operator ==(CodeGenEnumElement lhs, CodeGenEnumElement rhs)
+    {
+        if (lhs.Name != rhs.Name) return false;
+        return lhs.Value == rhs.Value;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is CodeGenEnumElement element && this == element;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Value);
+    }
+}
