9c983c0393a3468b6b63ed25bae388b8989006a4
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-04-30 18:19:05 -0700

    Block scoped namespace for ParameterInfo

--- Il2CppInterop.Runtime/Runtime/VersionSpecific/ParameterInfo/ParameterInfo_16_0.cs
+++ Il2CppInterop.Runtime/Runtime/VersionSpecific/ParameterInfo/ParameterInfo_16_0.cs
@@ -1,55 +1,56 @@
 using System;
 using System.Runtime.InteropServices;
 
-namespace Il2CppInterop.Runtime.Runtime.VersionSpecific.ParameterInfo;
-
-[ApplicableToUnityVersionsSince("5.3.0")]
-internal class NativeParameterInfoStructHandler_16_0 : INativeParameterInfoStructHandler
+namespace Il2CppInterop.Runtime.Runtime.VersionSpecific.ParameterInfo
 {
-    public unsafe int Size()
+    [ApplicableToUnityVersionsSince("5.3.0")]
+    internal class NativeParameterInfoStructHandler_16_0 : INativeParameterInfoStructHandler
     {
-        return sizeof(Il2CppParameterInfo_16_0);
-    }
-
-    public unsafe INativeParameterInfoStruct? Wrap(Il2CppParameterInfo* paramInfoPointer)
-    {
-        if ((IntPtr)paramInfoPointer == IntPtr.Zero) return null;
-        return new NativeParameterInfoStructWrapper((IntPtr)paramInfoPointer);
-    }
-
-    public bool HasNamePosToken => true;
-
-    [StructLayout(LayoutKind.Sequential)]
-    internal unsafe struct Il2CppParameterInfo_16_0
-    {
-        public IntPtr name; // const char*
-        public int position;
-        public uint token;
-        public int customAttributeIndex;
-        public Il2CppTypeStruct* parameter_type; // const
-    }
-
-    internal unsafe class NativeParameterInfoStructWrapper : INativeParameterInfoStruct
-    {
-        public NativeParameterInfoStructWrapper(IntPtr pointer)
+        public unsafe int Size()
         {
-            Pointer = pointer;
+            return sizeof(Il2CppParameterInfo_16_0);
         }
 
-        private Il2CppParameterInfo_16_0* NativeParameter => (Il2CppParameterInfo_16_0*)Pointer;
-
-        public IntPtr Pointer { get; }
-
-        public Il2CppParameterInfo* ParameterInfoPointer => (Il2CppParameterInfo*)Pointer;
+        public unsafe INativeParameterInfoStruct? Wrap(Il2CppParameterInfo* paramInfoPointer)
+        {
+            if ((IntPtr)paramInfoPointer == IntPtr.Zero) return null;
+            return new NativeParameterInfoStructWrapper((IntPtr)paramInfoPointer);
+        }
 
         public bool HasNamePosToken => true;
 
-        public ref IntPtr Name => ref NativeParameter->name;
+        [StructLayout(LayoutKind.Sequential)]
+        internal unsafe struct Il2CppParameterInfo_16_0
+        {
+            public IntPtr name; // const char*
+            public int position;
+            public uint token;
+            public int customAttributeIndex;
+            public Il2CppTypeStruct* parameter_type; // const
+        }
 
-        public ref int Position => ref NativeParameter->position;
+        internal unsafe class NativeParameterInfoStructWrapper : INativeParameterInfoStruct
+        {
+            public NativeParameterInfoStructWrapper(IntPtr pointer)
+            {
+                Pointer = pointer;
+            }
 
-        public ref uint Token => ref NativeParameter->token;
+            private Il2CppParameterInfo_16_0* NativeParameter => (Il2CppParameterInfo_16_0*)Pointer;
 
-        public ref Il2CppTypeStruct* ParameterType => ref NativeParameter->parameter_type;
+            public IntPtr Pointer { get; }
+
+            public Il2CppParameterInfo* ParameterInfoPointer => (Il2CppParameterInfo*)Pointer;
+
+            public bool HasNamePosToken => true;
+
+            public ref IntPtr Name => ref NativeParameter->name;
+
+            public ref int Position => ref NativeParameter->position;
+
+            public ref uint Token => ref NativeParameter->token;
+
+            public ref Il2CppTypeStruct* ParameterType => ref NativeParameter->parameter_type;
+        }
     }
 }
