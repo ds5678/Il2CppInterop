948230fad58db9ba4611054ac1572699413582ad
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-04-30 18:19:05 -0700

    Block scoped namespace for ParameterInfo

--- Il2CppInterop.Runtime/Runtime/VersionSpecific/ParameterInfo/ParameterInfo_27_0.cs
+++ Il2CppInterop.Runtime/Runtime/VersionSpecific/ParameterInfo/ParameterInfo_27_0.cs
@@ -1,52 +1,53 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace Il2CppInterop.Runtime.Runtime.VersionSpecific.ParameterInfo;
-
-[ApplicableToUnityVersionsSince("2021.2.0")]
-internal class NativeParameterInfoStructHandler_27_0 : INativeParameterInfoStructHandler
+namespace Il2CppInterop.Runtime.Runtime.VersionSpecific.ParameterInfo
 {
-    public unsafe int Size()
+    [ApplicableToUnityVersionsSince("2021.2.0")]
+    internal class NativeParameterInfoStructHandler_27_0 : INativeParameterInfoStructHandler
     {
-        return sizeof(Il2CppParameterInfo_27_0);
-    }
-
-    public unsafe INativeParameterInfoStruct? Wrap(Il2CppParameterInfo* paramInfoPointer)
-    {
-        if ((IntPtr)paramInfoPointer == IntPtr.Zero) return null;
-        return new NativeParameterInfoStructWrapper((IntPtr)paramInfoPointer);
-    }
-
-    public bool HasNamePosToken => false;
-
-    //Doesn't actually exist; just using this for type pointer storage in MethodInfo 27_3 +
-    [StructLayout(LayoutKind.Sequential)]
-    internal unsafe struct Il2CppParameterInfo_27_0
-    {
-        public Il2CppTypeStruct* parameter_type;
-    }
-
-    internal unsafe class NativeParameterInfoStructWrapper : INativeParameterInfoStruct
-    {
-        public NativeParameterInfoStructWrapper(IntPtr pointer)
+        public unsafe int Size()
         {
-            Pointer = pointer;
+            return sizeof(Il2CppParameterInfo_27_0);
         }
 
-        private Il2CppParameterInfo_27_0* NativeParameter => (Il2CppParameterInfo_27_0*)Pointer;
-
-        public IntPtr Pointer { get; }
-
-        public Il2CppParameterInfo* ParameterInfoPointer => (Il2CppParameterInfo*)Pointer;
+        public unsafe INativeParameterInfoStruct? Wrap(Il2CppParameterInfo* paramInfoPointer)
+        {
+            if ((IntPtr)paramInfoPointer == IntPtr.Zero) return null;
+            return new NativeParameterInfoStructWrapper((IntPtr)paramInfoPointer);
+        }
 
         public bool HasNamePosToken => false;
 
-        public ref IntPtr Name => throw new NotSupportedException("ParameterInfo does not exist in Unity 2021.2.0+");
+        //Doesn't actually exist; just using this for type pointer storage in MethodInfo 27_3 +
+        [StructLayout(LayoutKind.Sequential)]
+        internal unsafe struct Il2CppParameterInfo_27_0
+        {
+            public Il2CppTypeStruct* parameter_type;
+        }
 
-        public ref int Position => throw new NotSupportedException("ParameterInfo does not exist in Unity 2021.2.0+");
+        internal unsafe class NativeParameterInfoStructWrapper : INativeParameterInfoStruct
+        {
+            public NativeParameterInfoStructWrapper(IntPtr pointer)
+            {
+                Pointer = pointer;
+            }
 
-        public ref uint Token => throw new NotSupportedException("ParameterInfo does not exist in Unity 2021.2.0+");
+            private Il2CppParameterInfo_27_0* NativeParameter => (Il2CppParameterInfo_27_0*)Pointer;
 
-        public ref Il2CppTypeStruct* ParameterType => ref NativeParameter->parameter_type;
+            public IntPtr Pointer { get; }
+
+            public Il2CppParameterInfo* ParameterInfoPointer => (Il2CppParameterInfo*)Pointer;
+
+            public bool HasNamePosToken => false;
+
+            public ref IntPtr Name => throw new NotSupportedException("ParameterInfo does not exist in Unity 2021.2.0+");
+
+            public ref int Position => throw new NotSupportedException("ParameterInfo does not exist in Unity 2021.2.0+");
+
+            public ref uint Token => throw new NotSupportedException("ParameterInfo does not exist in Unity 2021.2.0+");
+
+            public ref Il2CppTypeStruct* ParameterType => ref NativeParameter->parameter_type;
+        }
     }
 }
