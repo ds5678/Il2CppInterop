e4d65d043de9936beb25d68decb33abddca3e820
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-04-30 20:28:25 -0700

    Struct generator performance improvements

--- Il2CppInterop.StructGenerator/CodeGen/ElementProtection.cs
+++ Il2CppInterop.StructGenerator/CodeGen/ElementProtection.cs
@@ -7,3 +7,14 @@ internal enum ElementProtection
     Internal,
     Public
 }
+internal static class ElementProtectionExtensions
+{
+    public static string ToCSharpString(this ElementProtection value) => value switch
+    {
+        ElementProtection.Private => "private",
+        ElementProtection.Protected => "protected",
+        ElementProtection.Internal => "internal",
+        ElementProtection.Public => "public",
+        _ => throw new InvalidOperationException($"Unknown ElementProtection value: {value}")
+    };
+}
