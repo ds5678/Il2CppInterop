75865b66d563409100291821a1d8696bb27ef90f
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-05-07 20:26:04 -0700

    Enable operation without assemblies for unstripping

--- Il2CppInterop.Generator/MscorlibAssemblyInjectionProcessingLayer.cs
+++ Il2CppInterop.Generator/MscorlibAssemblyInjectionProcessingLayer.cs
@@ -10,45 +10,76 @@ public class MscorlibAssemblyInjectionProcessingLayer : UnstripBaseProcessingLay
 
     public override string Id => "mscorlib_injector";
 
+    private static readonly string[] injectedAssemblies =
+    [
+        "mscorlib",
+        "System.Collections",
+    ];
+    internal static ReadOnlySpan<string> InjectedAssemblies => injectedAssemblies;
+
     public override void Process(ApplicationAnalysisContext appContext, Action<int, int>? progressCallback = null)
     {
-        var assemblyList = appContext.GetExtraData<IReadOnlyList<AssemblyDefinition>>(AssembliesKey);
-        if (assemblyList is null)
-        {
-            var directoryPath = appContext.GetExtraData<string>(DirectoryKey);
-            if (directoryPath is null)
-            {
-                Logger.WarnNewline($"No assemblies specified - processor will not run. You need to provide the {DirectoryKey}, either by programmatically adding it as extra data in the app context, or by specifying it in the --processor-config command line option.", nameof(UnstripProcessingLayer));
-                return;
-            }
-
-            assemblyList = Directory.GetFiles(directoryPath, "*.dll", SearchOption.AllDirectories)
-                .Where(x => x.Contains("mscorlib", StringComparison.OrdinalIgnoreCase))
-                .Select(path => AssemblyDefinition.FromFile(path))
-                .ToList();
-        }
-
-        var mscorlib = assemblyList.FirstOrDefault(x => x.Name == "mscorlib");
-
-        if (mscorlib is null)
-        {
-            Logger.WarnNewline("mscorlib not provided - processor will not run.", nameof(UnstripProcessingLayer));
-            return;
-        }
-
-        if (appContext.AssembliesByName.ContainsKey("mscorlib"))
-        {
-            Logger.WarnNewline("mscorlib already injected - processor will not run.", nameof(MscorlibAssemblyInjectionProcessingLayer));
-            return;
-        }
-
         Logger.InfoNewline($"Injecting new mscorlib...", nameof(MscorlibAssemblyInjectionProcessingLayer));
 
-        InjectAssemblies(appContext, [mscorlib], false, true);
+        var mscorlib = LoadSystemRuntime();
+        var runtimeContext = new RuntimeContext(DotNetRuntimeInfo.NetCoreApp(10, 0), new AssemblyResolver(), mscorlib);
+        runtimeContext.AddAssembly(mscorlib);
+
+        var assembliesToInject = new AssemblyDefinition[injectedAssemblies.Length];
+        assembliesToInject[0] = mscorlib;
+        for (var i = 1; i < injectedAssemblies.Length; i++)
+        {
+            var assemblyName = injectedAssemblies[i];
+            assembliesToInject[i] = LoadAssembly(assemblyName, runtimeContext);
+        }
+
+        InjectAssemblies(appContext, assembliesToInject, false, true);
 
         // Need to reset the system types context to use the new corlib
         appContext.SystemTypes = new SystemTypesContext(appContext);
 
-        appContext.AssembliesByName["mscorlib"].IsReferenceAssembly = true;
+        foreach (var assemblyName in injectedAssemblies)
+        {
+            appContext.AssembliesByName[assemblyName].IsReferenceAssembly = true;
+        }
+    }
+
+    private static AssemblyDefinition LoadSystemRuntime()
+    {
+        AssemblyDefinition assembly = AssemblyDefinition.FromBytes(Basic.Reference.Assemblies.Net100.ReferenceInfos.SystemRuntime.ImageBytes, createRuntimeContext: false);
+        assembly.Name = "mscorlib";
+        assembly.ManifestModule!.Name = "mscorlib.dll";
+        return assembly;
+    }
+
+    private static AssemblyDefinition LoadAssembly(string assemblyName, RuntimeContext runtimeContext)
+    {
+        var assembly = AssemblyDefinition.FromBytes(GetAssemblyBytes($"{assemblyName}.dll"), createRuntimeContext: false);
+        foreach (var module in assembly.Modules)
+        {
+            foreach (var assemblyReference in module.AssemblyReferences)
+            {
+                if (assemblyReference.Name == "System.Runtime")
+                {
+                    assemblyReference.Name = "mscorlib";
+                }
+            }
+        }
+        runtimeContext.AddAssembly(assembly);
+        return assembly;
+    }
+
+    private static byte[] GetAssemblyBytes(string fileName)
+    {
+        return Basic.Reference.Assemblies.Net100.ReferenceInfos.All.First(x => x.FileName == fileName).ImageBytes;
+    }
+
+    private sealed class AssemblyResolver : IAssemblyResolver
+    {
+        public ResolutionStatus Resolve(AssemblyDescriptor assembly, ModuleDefinition? originModule, out AssemblyDefinition? result)
+        {
+            result = null;
+            return ResolutionStatus.AssemblyNotFound;
+        }
     }
 }
