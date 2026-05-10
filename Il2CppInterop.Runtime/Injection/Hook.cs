d372692ee979f453fcd618da0940dd11b7068a0a
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-05-10 10:52:27 -0700

    File scoped namespace for hooking classes

--- Il2CppInterop.Runtime/Injection/Hook.cs
+++ Il2CppInterop.Runtime/Injection/Hook.cs
@@ -3,44 +3,43 @@ using System.Runtime.InteropServices;
 using Il2CppInterop.Common;
 using Microsoft.Extensions.Logging;
 
-namespace Il2CppInterop.Runtime.Injection
+namespace Il2CppInterop.Runtime.Injection;
+
+internal abstract class Hook<T> where T : Delegate
 {
-    internal abstract class Hook<T> where T : Delegate
+    private bool _isApplied;
+    private T _detour;
+    private T _method;
+    private T _original;
+
+    public T Original => _original;
+
+    public abstract string TargetMethodName { get; }
+    public abstract T GetDetour();
+    public abstract IntPtr FindTargetMethod();
+
+    public virtual void TargetMethodNotFound()
     {
-        private bool _isApplied;
-        private T _detour;
-        private T _method;
-        private T _original;
+        throw new Exception($"Required target method {TargetMethodName} not found");
+    }
 
-        public T Original => _original;
+    public void ApplyHook()
+    {
+        if (_isApplied) return;
 
-        public abstract string TargetMethodName { get; }
-        public abstract T GetDetour();
-        public abstract IntPtr FindTargetMethod();
+        var methodPtr = FindTargetMethod();
 
-        public virtual void TargetMethodNotFound()
+        if (methodPtr == IntPtr.Zero)
         {
-            throw new Exception($"Required target method {TargetMethodName} not found");
+            TargetMethodNotFound();
+            return;
         }
 
-        public void ApplyHook()
-        {
-            if (_isApplied) return;
+        Logger.Instance.LogTrace("{MethodName} found: 0x{MethodPtr}", TargetMethodName, methodPtr.ToInt64().ToString("X2"));
 
-            var methodPtr = FindTargetMethod();
-
-            if (methodPtr == IntPtr.Zero)
-            {
-                TargetMethodNotFound();
-                return;
-            }
-
-            Logger.Instance.LogTrace("{MethodName} found: 0x{MethodPtr}", TargetMethodName, methodPtr.ToInt64().ToString("X2"));
-
-            _detour = GetDetour();
-            Detour.Apply(methodPtr, _detour, out _original);
-            _method = Marshal.GetDelegateForFunctionPointer<T>(methodPtr);
-            _isApplied = true;
-        }
+        _detour = GetDetour();
+        Detour.Apply(methodPtr, _detour, out _original);
+        _method = Marshal.GetDelegateForFunctionPointer<T>(methodPtr);
+        _isApplied = true;
     }
 }
