b2e6c0ff9a9ca023c64ddd92fa1a65f01aabc9a2
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-05-10 23:12:37 -0700

    Unify trampoline creation

--- Il2CppInterop.Runtime/InteropTypes/Il2CppToMonoDelegateReference.cs
+++ Il2CppInterop.Runtime/InteropTypes/Il2CppToMonoDelegateReference.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 using Il2CppInterop.Common;
 using Il2CppInterop.Common.Attributes;
@@ -30,5 +34,55 @@ internal sealed partial class Il2CppToMonoDelegateReference : Object
     partial void LogErrorIl2CppFinalize(Exception exception)
     {
         Logger.Instance.LogError($"Exception in {nameof(Il2CppToMonoDelegateReference)}.{nameof(Il2CppFinalize)}: {{Exception}}", exception);
+    }
+
+    private static ConcurrentDictionary<Type, MethodInfo> _methodInfoCache = new();
+
+    /// <summary>
+    /// Creates or retrieves a cached dynamic method that will cast <see cref="ReferencedDelegate"/>
+    /// to the specified <paramref name="delegateType"/> and invoke it with the provided arguments.
+    /// </summary>
+    /// <remarks>
+    /// Due to limitations in dynamic method creation, the dynamic method must be static.
+    /// As such, the first parameter of the dynamic method is always <see cref="Il2CppToMonoDelegateReference"/>.
+    /// The remaining parameters are copied from the Invoke method of <paramref name="delegateType"/>.
+    /// </remarks>
+    /// <param name="delegateType">The type of the delegate to cast and invoke.</param>
+    /// <returns>The dynamic method.</returns>
+    internal static MethodInfo GetOrCreateInvokeMethod(Type delegateType)
+    {
+        return _methodInfoCache.GetOrAdd(delegateType, CreateInvokeMethod);
+    }
+
+    private static MethodInfo CreateInvokeMethod(Type delegateType)
+    {
+        var delegateInvokeMethod = delegateType.GetMethod("Invoke")
+            ?? throw new InvalidOperationException($"Delegate type {delegateType.FullName} does not have an Invoke method.");
+
+        var returnType = delegateInvokeMethod.ReturnType;
+
+        // Dynamic methods must be static, so we prepend the declaring type as the first parameter
+        Type[] parameterTypes =
+        [
+            typeof(Il2CppToMonoDelegateReference),
+            ..delegateInvokeMethod.GetParameters().Select(p => p.ParameterType)
+        ];
+
+        var invokeMethod = new DynamicMethod($"Invoke_{delegateType.FullName}", returnType, parameterTypes,
+            typeof(Il2CppToMonoDelegateReference), false);
+        var bodyBuilder = invokeMethod.GetILGenerator();
+
+        bodyBuilder.Emit(OpCodes.Ldarg_0);
+        bodyBuilder.Emit(OpCodes.Callvirt, typeof(Il2CppToMonoDelegateReference).GetProperty(nameof(ReferencedDelegate))!.GetMethod!);
+        bodyBuilder.Emit(OpCodes.Castclass, delegateType);
+        for (var i = 1; i < parameterTypes.Length; i++)
+        {
+            bodyBuilder.Emit(OpCodes.Ldarg, i);
+        }
+
+        bodyBuilder.Emit(OpCodes.Callvirt, delegateInvokeMethod);
+        bodyBuilder.Emit(OpCodes.Ret);
+
+        return invokeMethod;
     }
 }
